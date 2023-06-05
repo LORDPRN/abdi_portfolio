2e17b604f001fbbec2b16c630a9ea72e91a1dca2
Abdiel Arias <abdiel.arias@uao.edu.co>
2023-06-05 16:19:16 -0500

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import Spline from '@splinetool/react-spline';
+import React, { Suspense, lazy } from 'react';
+
+// Importa el componente Spline usando lazy
+const LazySpline = lazy(() => import('@splinetool/react-spline'));
 
 const Computers = () => {
   return (
-    <Spline scene="https://prod.spline.design/AmBis8JxDSKSYXCy/scene.splinecode" />
+    <Suspense fallback={<div>Loading...</div>}>
+      {/* Renderiza el componente LazySpline */}
+      <LazySpline scene="https://prod.spline.design/AmBis8JxDSKSYXCy/scene.splinecode" />
+    </Suspense>
   );
 }
-export default Computers
+
+export default Computers;

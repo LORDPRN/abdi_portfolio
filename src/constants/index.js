e36c04e022f921e034a962923ffbec2f50d97486
Abdiel Arias <abdiel.arias@uao.edu.co>
2023-06-06 12:34:50 -0500

    Update index.js
    CV update, again.

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,7 +44,7 @@ import {
     {
       id: "cv",
       title: "CV 📄",
-      url: "https://drive.google.com/file/d/11qZZ08lSvXa602u6KtHMx4JL1pZjB623/view?usp=sharing"
+      url: "https://drive.google.com/file/d/1fNfE6GvhmoUahs11h5JKqTT6mnPe_Xpk/view?usp=sharing"
     }
   ];
   

68656a11d24c733499e139d2705b94234de2895e
Abdiel Arias <abdiel.arias@uao.edu.co>
2023-06-06 08:31:39 -0500

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,32 +1,42 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles"
-import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
 
 const Tech = () => {
+  const rotationSpeed = 0.1; // Velocidad de rotación
+
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Languages and tools
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
+          Technologies.
         </h2>
-    </motion.div>
+      </motion.div>
 
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) =>(
-        <div className="w-28 h-28" key={technology.name}>
-          <p className={`${styles.sectionSubText} text-center`}>{technology.name}</p>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div></>
+      <div className="mt-5 flex flex-row flex-wrap justify-center gap-5">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <p className={`${styles.sectionSubText} text-center`}>{technology.name}</p>
+            <motion.div
+              style={{ position: "relative" }} // Estilo para posicionar correctamente el icosahedro
+              initial={{ rotateY: 0 }} // Rotación inicial
+              animate={{ rotateY: 360 }} // Rotación final (completa vuelta)
+              transition={{ duration: 60 / rotationSpeed, repeat: Infinity }} // Duración de la animación y repetición infinita
+            >
+              <BallCanvas icon={technology.icon} />
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 

1498c46ae2c23f89906926b08d7c3da1c4d8f179
Abdiel Arias <abdiel.arias@uao.edu.co>
2023-06-06 08:22:12 -0500

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,33 +1,32 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles"
+import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          Languages and tools
+    <motion.div variants={textVariant()}>
+      <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Technologies.
+          Work Experience.
         </h2>
-      </motion.div>
+    </motion.div>
 
-      <div className="mt-10 flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <p className={`${styles.sectionSubText} text-center`}>{technology.name}</p>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) =>(
+        <div className="w-28 h-28" key={technology.name}>
+          <p className={`${styles.sectionSubText} text-center`}>{technology.name}</p>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div></>
   )
 }
 
